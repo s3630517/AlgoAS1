74765962d4bf0fd0bbda4ddf3123a80d14f2b9a8
rmit-s3540166-Han-Yang <s3540166@student.rmit.edu.au>
2019-08-19 07:23:06 +1000

    Add files via upload

--- AA_Assign1_2019S2/SequentialRepresentation.java
+++ AA_Assign1_2019S2/SequentialRepresentation.java
@@ -1,6 +1,8 @@
 import java.io.PrintWriter;
-
-
+import java.io.*;
+import java.util.*;
+import java.util.regex.Pattern;
+import java.util.Arrays;
 /**
  * Sequential Tree Representation implementation for the {@link BSPTree} interface.
  * <p>
@@ -14,42 +16,134 @@ public class SequentialRepresentation<T> implements BSPTree<T> {
 
     /**
      * Constructs empty graph.
-     */
-    public SequentialRepresentation() {
+     */ 
+	int size = 100;
+	T[] treeArray = (T[]) new Object[size];
+	T root;
+	T left, right;
+
+    public SequentialRepresentation() { 
         // Implement me!
+		root = null;
+        left = null;
+        right = null;
+		
+		
+		for(int i=0; i< treeArray.length;i++){
+			treeArray[i] = (T) EMPTY_NODE;
+		}
     } // end of SequentialRepresentation()
 
     @Override
     public void setRootNode(T nodeLabel) {
         // Implement me!
+		//String rootNode = (String) nodeLabel;
+		//treeArray[0] = rootNode;
+		if(treeArray[0]==EMPTY_NODE){
+			treeArray[0]= nodeLabel;
+			
+			System.out.println("testsetrootnode");
+		}else{
+			System.out.println("rootnodealreadyexists");
+		}
+		//System.out.println(Arrays.toString(treeArray));
     } // end of setRootNode()
 
     @Override
     public void splitNode(T srcLabel, T leftChild, T rightChild) {
         // Implement me!
+		int j=0;
+		for(int i=0; i<treeArray.length; i++) {
+			if(treeArray[i].equals(srcLabel)){
+				j = (i * 2) + 1;
+				treeArray[j]=leftChild;
+				treeArray[j+1]=rightChild;
+				//System.out.println("testif");
+				System.out.println(Arrays.toString(treeArray));
+				break;
+			}else {
+				//System.out.println("node not found" + treeArray[i]);
+			}
+			//System.out.println(treeArray[i]);
+		}
+		
     } // end of splitNode
 
     @Override
     public boolean findNode(T nodeLabel) {
         // Implement me!
+		for(int i=0; i<treeArray.length; i++) {
+			if(treeArray[i].equals(nodeLabel)){
+				//System.out.println("testfind "+treeArray[i]);
+				return true;
+			}
+		}
+		
         return false;
     } // end of findNode
 
     @Override
     public String findParent(T nodeLabel) {
         // Implement me!
+		String out = null;
+		for(int i=0; i<treeArray.length; i++) {
+			if(treeArray[(i * 2) + 1].equals(nodeLabel) || treeArray[(i * 2) + 2].equals(nodeLabel)){
+				out = nodeLabel+" "+ treeArray[i];
+				return out;
+			}
+		}
         return null;
     } // end of findParent
 
     @Override
     public String findChildren(T nodeLabel) {
         // Implement me!
+		int j=0;
+		String left;
+		String right;
+		String out;
+		for(int i=0; i<treeArray.length; i++) {
+			if(treeArray[i].equals(nodeLabel)){
+				j = (i * 2) + 1;
+				left=(String) treeArray[j];
+				right=(String) treeArray[j+1];
+				out = nodeLabel+" "+left+" "+right;
+				return out;
+			}else {
+				//System.out.println("node not found" + treeArray[i]);
+			}
+		}
         return null;
     } // end of findParent
-
+	
+	private void preOrder(T test) {
+		String in;
+		if (test == null) {
+			System.out.println("hello test");
+			return;
+		}
+		
+		in = findChildren(test);
+		String split[] = in.split(" ", 3);
+		//if(left.matches("[A-Z]{1}")){
+			left = (T)split[1];
+			//right =(T)split[2];
+			System.out.println("hello test2" + left);
+			//preOrder(left);
+		//}
+		//preOrder(right);
+	}
+	
     @Override
     public void printInPreorder(PrintWriter writer) {
         // Implement me!
+		root= treeArray[0];
+		preOrder(root);
+		
+		for(int i=0; i<treeArray.length; i++) {
+			writer.print(treeArray[i]);
+		}
+		
     } // end of printInPreorder
 
     @Override
